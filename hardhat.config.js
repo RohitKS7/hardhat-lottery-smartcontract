56268be8f85bc8ee75cd3c088a036f8cb70baeb3
Rohit kumar suman <rohitkumarsuman2011india@gmail.com>
2022-09-02 19:29:13 +0530

    nice

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,13 +21,12 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API k
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
-        hardhat: {
+        localhost: {
             // // If you want to do some forking, uncomment this
             // forking: {
             //   url: MAINNET_RPC_URL
             // }
             chainId: 31337,
-            blockConfirmations: 1,
         },
         goreli: {
             chainId: 5,
